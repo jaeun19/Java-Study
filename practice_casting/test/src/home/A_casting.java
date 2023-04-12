96e2aa6792b2ce8d64e44ddd4fc6a9b8c351b5b6
JAEUN_KIM <129568354+JaEunKimm@users.noreply.github.com>
2023-04-12 13:19:52 +0900

    Add files via upload

---
+++ practice_casting/test/src/home/A_casting.java
@@ -0,0 +1,69 @@
+package home;
+
+import java.util.Scanner;
+
+public class A_casting {
+	
+	
+	public void method1() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("국어 : ");
+		double lang = sc.nextDouble();
+		
+		System.out.print("영어 : ");
+		double eng = sc.nextDouble();
+		
+		System.out.print("수학 : ");
+		double math = sc.nextDouble();
+		
+		int sum = (int)(lang + eng + math);
+		int mean = sum / 3;
+		
+	    System.out.println("총점 : "+sum);
+	    System.out.println("평균 : "+mean);
+		
+	}
+	
+	public void method2() {
+		
+		int iNum1 = 10;
+		int iNum2= 4;
+		
+		float fNum = 3.0f;
+		
+		double dNum = 2.5;
+		
+		char ch = 'A';
+		
+		System.out.println( iNum1 / iNum2 );
+		System.out.println((int)dNum);
+		
+		System.out.println(iNum2 * dNum);
+		System.out.println((double)iNum1);
+		
+		System.out.println( (double)iNum1 / iNum2 );
+		System.out.println(dNum);
+		
+		System.out.println((int)fNum);
+		System.out.println(iNum1 / (int)fNum);
+		
+		System.out.println((float)iNum1 / fNum);
+		System.out.println((double)iNum1 / fNum);
+		
+		System.out.println(ch);
+		System.out.println((int)ch);
+		System.out.println((int)ch + iNum1);
+		System.out.println((char)(ch+iNum1));
+		
+		
+	}
+	
+
+
+	public static void main(String[] args) {
+		A_casting a= new A_casting();
+		//a.method1();
+	}
+
+}
